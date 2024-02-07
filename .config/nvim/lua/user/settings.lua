ea5d9e42b7013d3169c8033c509a47e6e60af722
emreenesyavuz <emre@eeyz.me>
2024-02-07 09:30:42 -0600

    Added WSL clipboard

--- .config/nvim/lua/user/settings.lua
+++ .config/nvim/lua/user/settings.lua
@@ -1,4 +1,4 @@
--- Modified: Wed 07 Feb 2024 09:28:52 AM CST
+-- Modified: Wed 07 Feb 2024 09:30:30 AM CST
 
 local set = vim.opt
 
@@ -82,4 +82,20 @@ group = modifiedtime,
 })
 
 
+vim.cmd [[
+if has('wsl')
+let g:clipboard = {
+    \   'name': 'WslClipboard',
+    \   'copy': {
+        \      '+': 'clip.exe',
+        \      '*': 'clip.exe',
+        \    },
+        \   'paste': {
+            \      '+': 'powershell.exe -c [Console]::Out.Write($(Get-Clipboard -Raw).tostring().replace("`r", ""))',
+            \      '*': 'powershell.exe -c [Console]::Out.Write($(Get-Clipboard -Raw).tostring().replace("`r", ""))',
+            \   },
+            \   'cache_enabled': 0,
+            \ }
+endif
+            ]]
 
